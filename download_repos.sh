79cc38cc021f9ba1e77e68f65271cfb922b5230b
Mariana Helena Inês Moreira <mariana.helena@inatel.br>
2021-10-26 14:47:07 -0300

    organizing selected repos

--- download_repos.sh
+++ download_repos.sh
@@ -7,87 +7,164 @@ echo "##################################################################"
 echo "         PySniffer - Downloading Repositories for Analysis        "
 echo "##################################################################"
 
-git clone https://github.com/ytdl-org/youtube-dl.git #project 102k stars
-git clone https://github.com/nvbn/thefuck.git #project 64.4k stars
-git clone https://github.com/pallets/flask.git #framework 57k stars
-git clone https://github.com/httpie/httpie.git #project 52.5k stars
-git clone https://github.com/home-assistant/core.git #project 46.8k stars
-git clone https://github.com/soimort/you-get.git #project 42k stars
-git clone https://github.com/scrapy/scrapy.git #framework 41.9k stars
-git clone https://github.com/apache/superset.git #app 41k stars
-git clone https://github.com/deepfakes/faceswap #tool 39k stars
-git clone https://github.com/3b1b/manim.git #project 38.3k stars
-git clone https://github.com/tiangolo/fastapi.git #framework 37.3k stars
-git clone https://github.com/localstack/localstack.git #emulator 35.9k stars
-git clone https://github.com/CorentinJ/Real-Time-Voice-Cloning.git #project 30.7k stars
-git clone https://github.com/commaai/openpilot #project 30.3k stars
-git clone https://github.com/getsentry/sentry.git #app 29.3k stars
-git clone https://github.com/certbot/certbot.git #tool 28.4k
-git clone https://github.com/sherlock-project/sherlock.git #project 27.5k stars
-git clone https://github.com/facebookresearch/Detectron.git #plataform 24.7k stars
-git clone https://github.com/trailofbits/algo.git #project 23.8k stars
-git clone https://github.com/apache/airflow.git #project 23.5k stars
-git clone https://github.com/ycm-core/YouCompleteMe.git #tool 23.4k stars
-git clone https://github.com/pypa/pipenv.git #tool 22.4k stars
-git clone https://github.com/encode/django-rest-framework.git #framework 22.1k stars
-git clone https://github.com/sqlmapproject/sqlmap.git #tool 21.4k stars
-git clone https://github.com/vinta/awesome-python.git #project 20.8k stars
-git clone https://github.com/tornadoweb/tornado.git  #framework 20.3k stars
-git clone https://github.com/beurtschipper/Depix.git #project 20.3k stars
-git clone https://github.com/tqdm/tqdm.git #project 19.8 stars
-git clone https://github.com/getredash/redash.git #project 19.7k stars
-git clone https://github.com/nicolargo/glances.git #tool 19.4k stars
-git clone https://github.com/facebookresearch/detectron2.git #plataform 18.5k stars
-git clone https://github.com/celery/celery.git #project 18.1k stars
-git clone https://github.com/ultralytics/yolov5.git #pesquisa 17.6k stars
-git clone https://github.com/locustio/locust.git #tool 17.4k stars
-git clone https://github.com/magenta/magenta.git #research project 17.1k stars
-git clone https://github.com/python-poetry/poetry.git #project 16.9k stars
-git clone https://github.com/open-mmlab/mmdetection.git #toolbox 16.9k stars
-git clone https://github.com/python-telegram-bot/python-telegram-bot.git #bot project 16.7k stars
-git clone https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix.git #project 16.1k stars
-git clone https://github.com/toml-lang/toml.git #project 16k stars
-git clone https://github.com/chubin/wttr.in.git #project 15.9k stars
-git clone https://github.com/tzutalin/labelImg.git #tool 15.6k stars
-git clone https://github.com/sanic-org/sanic.git #framework 15.5k stars
-git clone https://github.com/nginx-proxy/nginx-proxy.git #proxy 15.4k stars
-git clone https://github.com/mingrammer/diagrams.git #project 15.3k stars
-git clone https://github.com/plotly/dash.git #framework 15.3k stars
-git clone https://github.com/kivy/kivy.git #framework 13.6k stars
-git clone https://github.com/RasaHQ/rasa.git  #framework 12.9k stars
-git clone https://github.com/google/yapf.git #tool 12.1k stars
-git clone https://github.com/aio-libs/aiohttp.git #framework 11.8k stars
-git clone https://github.com/horovod/horovod.git #framework 11.8k stars
-git clone https://github.com/flairNLP/flair.git  #framework 10.9k stars
-git clone https://github.com/wifiphisher/wifiphisher.git #framework 10.2k stars
-git clone https://github.com/MobSF/Mobile-Security-Framework-MobSF.git #framework 9.9k stars
-git clone https://github.com/s0md3v/XSStrike.git #project 9.7k stars
-git clone https://github.com/falconry/falcon.git  #framework 8.6k stars
-git clone https://github.com/facebookresearch/ParlAI.git #framework 8.4k stars
-git clone https://github.com/pytest-dev/pytest.git  #framework 7.9k stars
-git clone https://github.com/bottlepy/bottle.git #framework 7.4k stars
-git clone https://github.com/encode/starlette.git #framework 6.2k stars
-git clone https://github.com/SerpentAI/SerpentAI.git #framework 6.1k stars
-git clone https://github.com/vibora-io/vibora.git #framework 5.7k stars
-git clone https://github.com/worldveil/dejavu.git #project 5.6k stars
-git clone https://github.com/webpy/webpy.git #framework 5.6k stars
-git clone https://github.com/burnash/gspread.git #API 5.6k stars
-git clone https://github.com/yzhao062/anomaly-detection-resources.git #project 5.1k stars
-git clone https://github.com/eternnoir/pyTelegramBotAPI.git #bot API 4.8k stars
-git clone https://github.com/sammchardy/python-binance.git #API 3.9k stars
-git clone https://github.com/rafaelpadilla/Object-Detection-Metrics.git #project 3.8k stars
-git clone https://github.com/shobrook/rebound.git #tool 3.7k stars
-git clone https://github.com/omab/python-social-auth #project 2.8k stars
-git clone https://github.com/lincolnloop/python-qrcode #project 2.7k stars
-git clone https://github.com/miguelgrinberg/python-socketio #project 2.6k stars
-git clone https://github.com/marblexu/PythonPlantsVsZombies.git #game 2.2k stars
-git clone https://github.com/henriquebastos/python-decouple.git #project 1.9k stars
-git clone https://github.com/google/python-adb.git #project 1.5k stars
+#Command-line program to download videos from YouTube.com and other video sites (102k stars)
+git clone https://github.com/ytdl-org/youtube-dl.git 
+#App which corrects your previous console command. (64.4k stars)
+git clone https://github.com/nvbn/thefuck.git 
+#Framework for building web applications. (57k stars)
+git clone https://github.com/pallets/flask.git 
+#Command-line HTTP client. (52.5k stars)
+git clone https://github.com/httpie/httpie.git 
+#Project: Open source home automation (46.8k stars)
+git clone https://github.com/home-assistant/core.git 
+#Command-line utility to download media contents from the Web (42k stars)
+git clone https://github.com/soimort/you-get.git 
+#Framework for web crawling and web scraping framework (41.9k stars)
+git clone https://github.com/scrapy/scrapy.git 
+#Web application: enterprise-ready business intelligence (41k stars)
+git clone https://github.com/apache/superset.git 
+#Tool that utilizes deep learning to recognize and swap faces in pictures and videos (39k stars)
+git clone https://github.com/deepfakes/faceswap  
+#Tool: Animation engine for explanatory math videos (38.3k stars)
+git clone https://github.com/3b1b/manim.git 
+#Web framework for building APIs (37.3k stars)
+git clone https://github.com/tiangolo/fastapi.git 
+#Emulator: local AWS cloud stack (35.9k stars)
+git clone https://github.com/localstack/localstack.git
+#Project: Real-Time Voice Cloning (30.7k stars)
+git clone https://github.com/CorentinJ/Real-Time-Voice-Cloning.git 
+#Project: open source driver assistance system (30.3k stars)
+git clone https://github.com/commaai/openpilot 
+#Service that helps you monitor and fix crashes in realtime (29.3k stars)
+git clone https://github.com/getsentry/sentry.git 
+#Tool to obtain certs from Let's Encrypt and auto-enable HTTPS on your server (28.4k)
+git clone https://github.com/certbot/certbot.git 
+#Project: Hunt down social media accounts by username across social networks (27.5k stars)
+git clone https://github.com/sherlock-project/sherlock.git 
+#Platform for object detection research (24.7k stars)
+git clone https://github.com/facebookresearch/Detectron.git 
+#Project: set of Ansible scripts that simplify the setup of a personal WireGuard and IPsec VPN (23.8k stars)
+git clone https://github.com/trailofbits/algo.git 
+#Platform to programmatically author, schedule, and monitor workflows (23.5k stars)
+git clone https://github.com/apache/airflow.git 
+#Tool: code-completion engine for Vim (23.4k stars)
+git clone https://github.com/ycm-core/YouCompleteMe.git 
+#Tool that aims to bring the best of all packaging worlds to the Python world (22.4k stars)
+git clone https://github.com/pypa/pipenv.git 
+#Framework: Web APIs for Django (22.1k stars)
+git clone https://github.com/encode/django-rest-framework.git 
+#Tool for automatic SQL injection and database takeover (21.4k stars)
+git clone https://github.com/sqlmapproject/sqlmap.git 
+#Web framework and asynchronous networking library (20.3k stars)
+git clone https://github.com/tornadoweb/tornado.git  
+#Tool: Recovers passwords from pixelized screenshots (20.3k stars)
+git clone https://github.com/beurtschipper/Depix.git 
+#Project: Progress Bar for Python and CLI (19.8 stars)
+git clone https://github.com/tqdm/tqdm.git 
+#Project: Connect to any data source, easily visualize, dashboard and share your data (19.7k stars)
+git clone https://github.com/getredash/redash.git 
+#Tool to present a large amount of monitoring information through a curses or Web based interface (19.4k stars)
+git clone https://github.com/nicolargo/glances.git 
+#Platform for object detection (18.5k stars)
+git clone https://github.com/facebookresearch/detectron2.git 
+#Project: distributed system to process vast amounts of messages (18.1k stars)
+git clone https://github.com/celery/celery.git  
+#Research into future vision AI methods, incorporating lessons learned and best practices (17.6k stars)
+git clone https://github.com/ultralytics/yolov5.git  
+#Tool: performance testing (17.4k stars)
+git clone https://github.com/locustio/locust.git  
+#Research project exploring the role of machine learning in the process of creating art and music (17.1k stars)
+git clone https://github.com/magenta/magenta.git 
+#Tool: Python dependency management and packaging (16.9k stars)
+git clone https://github.com/python-poetry/poetry.git  
+#Toolbox: object detection (16.9k stars)
+git clone https://github.com/open-mmlab/mmdetection.git  
+#Project: Image-to-Image Translation in PyTorch (16.1k stars)
+git clone https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix.git #project 
+#Project: minimal configuration file format (16k stars)
+git clone https://github.com/toml-lang/toml.git 
+#Project to check the weather (15.9k stars)
+git clone https://github.com/chubin/wttr.in.git  
+#Project: The lightweight PyTorch wrapper for high-performance AI research (15.8k stars)
+git clone https://github.com/PyTorchLightning/pytorch-lightning.git 
+#Tool for annotating images (15.6k stars)
+git clone https://github.com/tzutalin/labelImg.git 
+#Web server and Web framework (15.5k stars)
+git clone https://github.com/sanic-org/sanic.git  
+#Proxy: Automated nginx proxy for Docker containers (15.4k stars)
+git clone https://github.com/nginx-proxy/nginx-proxy.git  
+#Project: Diagrams lets you draw the cloud system architecture in Python code (15.3k stars)
+git clone https://github.com/mingrammer/diagrams.git 
+#Framework for building ML & data science web apps (15.3k stars)
+git clone https://github.com/plotly/dash.git  
+#Framework for the development of applications that make use of innovative, multi-touch user interfaces (13.6k stars)
+git clone https://github.com/kivy/kivy.git  
+#Framework to automate text- and voice-based conversations (12.9k stars)
+git clone https://github.com/RasaHQ/rasa.git   
+#Tool: a formatter for Python files (12.1k stars)
+git clone https://github.com/google/yapf.git  
+#Framework: Asynchronous HTTP client/server framework for asyncio and Python (11.8k stars)
+git clone https://github.com/aio-libs/aiohttp.git  
+#Framework: distributed deep learning training framework for TensorFlow, Keras, PyTorch, and Apache MXNet (11.8k stars)
+git clone https://github.com/horovod/horovod.git 
+#Framework for state-of-the-art Natural Language Processing (10.9k stars)
+git clone https://github.com/flairNLP/flair.git  
+#Framework:is a rogue Access Point framework for conducting red team engagements or Wi-Fi security testing (10.2k stars)
+git clone https://github.com/wifiphisher/wifiphisher.git  
+#Mobile Application: for malware analysis and security assessment (9.9k stars)
+git clone https://github.com/MobSF/Mobile-Security-Framework-MobSF.git 
+#Project: Advanced XSS Detection Suite (9.7k stars)
+git clone https://github.com/s0md3v/XSStrike.git  
+#Web framework for building large-scale app backends and microservices (8.6k stars)
+git clone https://github.com/falconry/falcon.git   
+#Framework for training and evaluating AI models on a variety of openly available dialogue datasets (8.4k stars)
+git clone https://github.com/facebookresearch/ParlAI.git  
+#Project: Incredibly fast crawler designed for OSINT (8.3k stars)
+git clone https://github.com/s0md3v/Photon.git 
+#Framework: makes it easy to write small tests, yet scales to support complex functional testing (7.9k stars)
+git clone https://github.com/pytest-dev/pytest.git  
+#Framework: a fast and simple micro-framework for python web-applications (7.4k stars)
+git clone https://github.com/bottlepy/bottle.git  
+#Framework/toolkit: a lightweight ASGI framework/toolkit, which is ideal for building high performance async services (6.2k stars)
+git clone https://github.com/encode/starlette.git 
+#Framework: Game Agent Framework. Helping you create AIs / Bots that learn to play any game you own (6.1k stars)
+git clone https://github.com/SerpentAI/SerpentAI.git  
+#Web framework (5.7k stars)
+git clone https://github.com/vibora-io/vibora.git  
+#Project: Audio fingerprinting and recognition algorithm (5.6k stars)
+git clone https://github.com/worldveil/dejavu.git 
+#Web framework (5.6k stars)
+git clone https://github.com/webpy/webpy.git  
+#API: Simple interface for working with Google Sheets (5.6k stars)
+git clone https://github.com/burnash/gspread.git  
+#API: Python Telegram bot api (4.8k stars)
+git clone https://github.com/eternnoir/pyTelegramBotAPI.git 
+#Project: Binance Exchange API python implementation for automated trading (3.9k stars)
+git clone https://github.com/sammchardy/python-binance.git  
+#Project: Most popular metrics used to evaluate object detection algorithms (3.8k stars)
+git clone https://github.com/rafaelpadilla/Object-Detection-Metrics.git 
+#Command-line tool that instantly fetches Stack Overflow results when an exception is thrown (3.7k stars)
+git clone https://github.com/shobrook/rebound.git  
+#Project: is an easy-to-setup social authentication/registration mechanism (2.8k stars)
+git clone https://github.com/omab/python-social-auth  
+#Project: Python QR Code image generator (2.7k stars)
+git clone https://github.com/lincolnloop/python-qrcode  
+#Project: Python implementation of the Socket.IO_ realtime client and server (2.6k stars)
+git clone https://github.com/miguelgrinberg/python-socketio #project
+#Game: PlantsVsZombies (2.2k stars)
+git clone https://github.com/marblexu/PythonPlantsVsZombies.git #game 
+#Project: to organize your settings so that you can change parameters without having to redeploy your app (1.9k stars)
+git clone https://github.com/henriquebastos/python-decouple.git #project
+#Project: pure-python implementation of the ADB and Fastboot protocols, using libusb1 for USB communications (1.5k stars)
+git clone https://github.com/google/python-adb.git #project 
 
 
 #------------------------------------------------------------------------------------
+#Library: that provides a pure Python interface for the Telegram Bot API (16.7k stars)
+#git clone https://github.com/python-telegram-bot/python-telegram-bot.git 
+
 #git clone https://github.com/binux/pyspider.git #15.2k stars
-#git clone https://github.com/PyTorchLightning/pytorch-lightning.git #15.8k stars
+
 #git clone https://github.com/bokeh/bokeh.git #library 15.6k stars
 #git clone https://github.com/deezer/spleeter.git #library 17.8k stars
 #git clone https://github.com/spotify/luigi.git #library 15.1k stars
@@ -98,15 +175,15 @@ git clone https://github.com/google/python-adb.git #project 1.5k stars
 #git clone https://github.com/willmcgugan/rich.git #library 30.4k stars
 #git clone https://github.com/pandas-dev/pandas.git #library 31.4k stars
 #git clone https://github.com/scikit-learn/scikit-learn.git #library 47.7k stars
-#git clone https://github.com/slackapi/bolt-python.git #382 stars
 #git clone https://github.com/sympy/sympy.git #library
 #git clone https://github.com/ageitgey/face_recognition.git #library 41.9k stars
 #git clone https://github.com/sloria/TextBlob.git  #library
 #git clone https://github.com/keras-team/keras.git #library 53k stars
-#git clone https://github.com/s0md3v/Photon.git #crawler / building failing
-#git clone https://github.com/d2l-ai/d2l-en.git #book
-#git clone https://github.com/BenAAndrew/Voice-Cloning-App.git #app / build failed
 #git clone https://github.com/keras-team/keras-applications.git #modulo da lib Keras
+
+
+#git clone https://github.com/BenAAndrew/Voice-Cloning-App.git # 434stars app / build failed
+#git clone https://github.com/slackapi/bolt-python.git #382 stars
 #git clone https://github.com/Ponytech/appstoreconnectapi.git #98 stars
 #git clone https://github.com/bigcommerce/hello-world-app-python-flask.git #30 stars
 #git clone https://github.com/Teraskull/PyDebloatX.git #GUI 625 stars
